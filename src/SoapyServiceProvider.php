2bcd30e5f2396592e757f5cd3196922b54cd4bcc
Connor Tumbleson <connor@sourcetoad.com>
2024-07-26 08:30:24 -0400

    chore: lint all files via pint + strict typing

--- src/SoapyServiceProvider.php
+++ src/SoapyServiceProvider.php
@@ -1,5 +1,6 @@
 <?php
-declare(strict_types = 1);
+
+declare(strict_types=1);
 
 namespace Sourcetoad\Soapy;
 
@@ -7,15 +8,15 @@ use Illuminate\Support\ServiceProvider;
 
 class SoapyServiceProvider extends ServiceProvider
 {
-    public function boot()
+    public function boot(): void
     {
         //
     }
 
-    public function register()
+    public function register(): void
     {
         $this->app->singleton(SoapyTub::class, function () {
-            return new SoapyTub();
+            return new SoapyTub;
         });
     }
 }
